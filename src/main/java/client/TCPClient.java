59fb32e9db82e4fd8eca1446750f8e27f0f8c14f
playerzer0-ui <playerzero745@gmail.com>
2024-03-14 14:58:36 +0000

    fix math

--- src/main/java/client/TCPClient.java
+++ src/main/java/client/TCPClient.java
@@ -11,37 +11,42 @@ import java.util.Scanner;
 
 public class TCPClient {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        boolean flag = false;
-        //request connection
-        while (!flag){
-            try(Socket socket = new Socket("localhost", TCProtocol.PORT)){
+        Scanner userInput = new Scanner(System.in);
+        // Requests a connection
+        try (Socket dataSocket = new Socket("localhost",TCProtocol.PORT)) {
 
-                //setup scanner and printWriter
-                try(Scanner input = new Scanner(socket.getInputStream()); PrintWriter output = new PrintWriter(socket.getOutputStream())){
-                    //get user to type something
-                    System.out.println("Please enter a message to be sent:");
-                    String msg = sc.nextLine();
-
-                    //throw it to the server
-                    output.println(msg);
+            // Sets up communication lines
+            // Create a Scanner to receive messages
+            // Create a Printwriter to send messages
+            try (Scanner input = new Scanner(dataSocket.getInputStream());
+                 PrintWriter output = new PrintWriter(dataSocket.getOutputStream())) {
+                boolean validSession = true;
+                // Repeated:
+                while(validSession) {
+                    // Ask user for information to be sent
+                    System.out.println("Please enter a message to be sent (Send EXIT to end):");
+                    String message = userInput.nextLine();
+                    // Send message to server
+                    output.println(message);
+                    // Flush message through to server
                     output.flush();
 
-                    //get the message from server
+                    // Receive message from server
                     String response = input.nextLine();
-                    System.out.println(response);
-
+                    // Display result to user
+                    System.out.println("Received from server: " + response);
                     if(response.equals(TCProtocol.EXIT)){
-                        flag = true;
+                        validSession = false;
                     }
                 }
+
             }
-            catch (UnknownHostException e) {
-                System.out.println("Host cannot be found at this moment. Try again later");
-            }
-            catch (IOException e) {
-                System.out.println("An IO Exception occurred: " + e.getMessage());
-            }
+
+        } catch (UnknownHostException e) {
+            System.out.println("Host cannot be found at this moment. Try again later");
+        } catch (IOException e) {
+            System.out.println("An IO Exception occurred: " + e.getMessage());
         }
+        // Close connection to server
     }
 }
--- src/main/java/server/TCPMathServer.java
+++ src/main/java/server/TCPMathServer.java
@@ -11,64 +11,100 @@ import java.util.Scanner;
 
 public class TCPMathServer {
     public static void main(String[] args) {
-        String response = null;
-        Map<Integer, Integer> map = new HashMap<>();
-
-        try (ServerSocket serverSocket = new ServerSocket(TCProtocol.PORT)){
-            while(true){
-                try(Socket socket = serverSocket.accept()){
-                    try(Scanner input = new Scanner(socket.getInputStream()); PrintWriter output = new PrintWriter(socket.getOutputStream())){
-                        String request = input.nextLine();
-
-                        System.out.println(request);
-                        String[] components = request.split(TCProtocol.DELIMITER);
-
-                        switch (components[0]){
-                            case TCProtocol.SQUARE:
+        // SET UP HOST AND PORT INFO
+        // Done in MathsService utility class
+        // Make a listening socket
+        try (ServerSocket listeningSocket = new ServerSocket(TCProtocol.PORT)) {
+            int largest = Integer.MIN_VALUE;
+            // REPEATEDLY:
+            while (true) {
+                // Accept an incoming connection request
+                Socket dataSocket = listeningSocket.accept();
+                // Set up our lines of communication - input and output
+                try (Scanner input = new Scanner(dataSocket.getInputStream());
+                     PrintWriter output = new PrintWriter(dataSocket.getOutputStream());) {
+                    boolean validSession = true;
+                    int clientLargest = Integer.MIN_VALUE;
+                    // REPEATEDLY
+                    while(validSession){
+                        // Take in a request
+                        String message = input.nextLine();
+                        // Parse the request
+                        String [] components = message.split(TCProtocol.DELIMITER);
+                        // Do the requested action and generate an appropriate response
+                        String response = "";
+                        switch(components[0]){
+                            case(TCProtocol.CUBE):
                                 if(components.length == 2){
                                     if(isNumber(components[1])){
-                                        int res = Integer.parseInt(components[1]);
-                                        response = String.valueOf(res * res);
+                                        int result = Integer.parseInt(components[1]);
+                                        response = String.valueOf(result * result);
+
+                                        if(result > clientLargest){
+                                            clientLargest = result;
+                                        }
+                                        if(result > largest){
+                                            largest = result;
+                                        }
                                     }
-                                    else {
-                                        response = TCProtocol.PLEASE_SUPPLY_NUMBER;
-                                    }
-                                }
-                                else{
-                                    response = TCProtocol.PLEASE_SUPPLY_NUMBER;
+                                }else{
+                                    response = TCProtocol.INVALID;
                                 }
                                 break;
-
-                            case TCProtocol.CUBE:
+                            case(TCProtocol.SQUARE):
                                 if(components.length == 2){
                                     if(isNumber(components[1])){
-                                        int res = Integer.parseInt(components[1]);
-                                        response = String.valueOf(res * res * res);
+                                        int result = Integer.parseInt(components[1]);
+                                        response = String.valueOf(result * result * result);
+
+                                        if(result > clientLargest){
+                                            clientLargest = result;
+                                        }
+                                        if(result > largest){
+                                            largest = result;
+                                        }
                                     }
-                                    else {
-                                        response = TCProtocol.PLEASE_SUPPLY_NUMBER;
-                                    }
-                                }
-                                else{
-                                    response = TCProtocol.PLEASE_SUPPLY_NUMBER;
+                                }else{
+                                    response = TCProtocol.INVALID;
                                 }
                                 break;
-
-                            case TCProtocol.MYLARGEST:
+                            case(TCProtocol.MYLARGEST):
+                                if(components.length == 1) {
+                                    response = "" + clientLargest;
+                                }else{
+                                    response = TCProtocol.INVALID;
+                                }
                                 break;
-
-
+                            case(TCProtocol.LARGEST):
+                                if(components.length == 1) {
+                                    response = "" + largest;
+                                }else{
+                                    response = TCProtocol.INVALID;
+                                }
+                                break;
+                            case(TCProtocol.EXIT):
+                                if(components.length == 1) {
+                                    validSession = false;
+                                    response = TCProtocol.EXIT;
+                                }else{
+                                    response = TCProtocol.INVALID;
+                                }
+                                break;
+                            default:
+                                response = TCProtocol.INVALID;
                         }
-
+                        // Send the response
                         output.println(response);
                         output.flush();
                     }
                 }
             }
-        }catch (BindException e) {
-            System.out.println("An exception occurred when binding to the server port " + TCProtocol.PORT);
+        } catch (BindException e) {
+            System.out.println("BindException occurred when attempting to bind to port " + TCProtocol.PORT);
+            System.out.println(e.getMessage());
         } catch (IOException e) {
-            System.out.println("An IO exception occurred" + e.getMessage());
+            System.out.println("IOException occurred on server socket");
+            System.out.println(e.getMessage());
         }
     }
     public static boolean isNumber(String input) {
